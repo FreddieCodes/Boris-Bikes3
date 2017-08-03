f4a19e2702fb48172ca1818fde42f771620b5200
stephengeller <stephen.geller@hotmail.com>
2017-08-03 16:08:33 +0100

    Added complex attributes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,24 +1,49 @@
 class DockingStation
-
   attr_reader :bikes
+
+  # constants
+  $DEFAULT_CAPACITY = 20
 
   def initialize
     @bikes = []
   end
 
-  def release_bike
+  def release_bike(count = 1)
+
+    released_bikes = []
+
     raise 'No bikes available' if @bikes.empty?
-    Bike.new
+
+    @bikes.each_with_index do |bike, index|
+      raise 'No bikes available' if @bikes.empty?
+      if bike.working?
+        released_bikes << bike
+        @bikes.delete_at index
+
+        if released_bikes.length == count
+          puts "#{released_bikes.length} bikes released from the station"
+          puts "#{@bikes.length} bikes left in the sation"
+          return released_bikes
+        end
+      end
+    end
+    raise 'No bikes available'
   end
 
   def dock(bike)
-    raise "Docking Station full" if @bikes.length >= 10
+    raise 'Docking Station full' if @bikes.length >= 20
     @bikes << bike if bike.class == Bike
     return bike
   end
 
   def show_bikes
     @bikes
+  end
+
+  def stock_bikes(n = $DEFAULT_CAPACITY)
+    n = ($DEFAULT_CAPACITY - @bikes.length) if (n+@bikes.length) > $DEFAULT_CAPACITY
+    n.times {@bikes << Bike.new}
+    "#{n} new bike#{"s" if n>1} have been stocked into the docking station"
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,32 +1,32 @@
-require "docking_station.rb"
+require 'docking_station.rb'
 
 describe DockingStation do
   it {expect(subject).to respond_to :release_bike}
 
-  it "gets a bike" do
+  it 'gets a bike' do
     subject.dock(Bike.new)
     bike = subject.release_bike
     expect(bike).to be_truthy
   end
 
-  it "can dock a bike" do
+  it 'can dock a bike' do
     expect(subject).to respond_to :dock
   end
 
-  it "docks specific bike" do
+  it 'docks specific bike' do
     bike = Bike.new
     expect(subject.dock(bike)). to eq(bike)
   end
 
   it {expect(subject).to respond_to :show_bikes}
 
-  it "shows a bike" do
+  it 'shows a bike' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.show_bikes).to include bike
   end
 
-  it "shows all bikes" do
+  it 'shows all bikes' do
     10.times {subject.dock(Bike.new)}
     expect(subject.show_bikes.length).to eq(10)
   end
@@ -35,8 +35,8 @@ describe DockingStation do
     expect{subject.release_bike}.to raise_error('No bikes available')
   end
 
-  it "can not dock when station is full" do
-    10.times {subject.dock(Bike.new)}
-    expect{subject.dock(Bike.new)}.to raise_error("Docking Station full")
+  it 'can not dock when station is full' do
+    20.times {subject.dock(Bike.new)}
+    expect{subject.dock(Bike.new)}.to raise_error('Docking Station full')
   end
 end
