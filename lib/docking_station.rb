e36f971ee2e9b23f620e89cde2551b53ca6b7311
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-03 12:48:15 +0100

    Added instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,22 @@
 class DockingStation
+
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bikes << bike
+   return bike
+  end
+
+  def show_bikes
+    @bikes
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,35 @@ require "docking_station.rb"
 
 describe DockingStation do
   it {expect(subject).to respond_to :release_bike}
-  end
 
-describe DockingStation do
   it "gets a bike" do
     bike = subject.release_bike
     expect(bike).to be_truthy
+  end
+
+  it "can dock a bike" do
+    expect(subject).to respond_to :dock
+  end
+
+  it "docks specific bike" do
+    bike = Bike.new
+    expect(subject.dock(bike)). to eq(bike)
+  end
+
+  it {expect(subject).to respond_to :show_bikes}
+
+  it "shows a bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.show_bikes).to include bike
+  end
+
+  it "shows all bikes" do
+    10.times {subject.dock(Bike.new)}
+    expect(subject.show_bikes.length).to eq(10)
+  end
+
+  
 
 
-  #it "expects the bike to be working" do
-  #end
-end
 end
