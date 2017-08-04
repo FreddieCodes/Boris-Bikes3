a69cb0348b99d25c7e422b185ff55022457a6716
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-04 16:35:07 +0100

    Mocking Behaviour on Doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,26 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+  let(:bike) { double :bike }
+  # If you try to create a broken bike you'll need to create
+  # another let. See below.
+  let(:broken_bike) { double :bike }
+
   it {expect(subject).to respond_to :release_bike}
 
   it 'gets a bike' do
-    subject.dock(Bike.new)
-    bike = subject.release_bike
-    expect(bike).to be_truthy
+    # Can out it into a kind of hash
+    allow(bike).to receive_messages(
+    #  :report_broken => nil,
+      :working? => true,
+      :class => Bike)
+    # or
+    # allow(bike).to receive(:report_broken)
+    # allow(bike).to receive(:working?).and_return(true)
+    # allow(bike).to receive(:class).and_return(Bike)
+
+    subject.dock(bike)
+    expect(subject.release_bike).to be_truthy
   end
 
   it 'can dock a bike' do
@@ -14,20 +28,26 @@ describe DockingStation do
   end
 
   it 'docks specific bike' do
-    bike = Bike.new
+    bike = double(:bike)
     expect(subject.dock(bike)). to eq(bike)
   end
 
   it {expect(subject).to respond_to :show_bikes}
 
   it 'shows a bike' do
-    bike = Bike.new
+    allow(bike).to receive_messages(
+      :class => Bike
+    )
     subject.dock(bike)
     expect(subject.show_bikes).to include bike
   end
 
   it 'shows all bikes' do
-    10.times {subject.dock(Bike.new)}
+    allow(bike).to receive_messages(
+      # :report_broken => nil,
+      # :working? => true,
+      :class => Bike)
+    10.times {subject.dock bike}
     expect(subject.show_bikes.length).to eq(10)
   end
 
@@ -36,12 +56,19 @@ describe DockingStation do
   end
 
   it 'can not dock when station is full' do
-    20.times {subject.dock(Bike.new)}
-    expect{subject.dock(Bike.new)}.to raise_error('Docking Station full')
+    allow(bike). to receive_messages(
+      :class => Bike
+    )
+    20.times {subject.dock bike}
+    expect{subject.dock(bike)}.to raise_error('Docking Station full')
   end
 
   it "can release multiple bikes" do
-    3.times {subject.dock(Bike.new)}
+    allow(bike). to receive_messages(
+      :class => Bike,
+      :working? => true
+    )
+    3.times {subject.dock bike}
     subject.release_bike(2)
     expect(subject.bikes.length).to eq(1)
   end
@@ -60,14 +87,23 @@ describe DockingStation do
   end
 
   it 'can report a bike as broken' do
-    bike = Bike.new
+    allow(bike). to receive_messages(
+      :report_broken => nil,
+      :class => Bike
+    )
+    # bike = double(:bike)
     expect(bike).to respond_to :report_broken
   end
 
   it 'does not release broken bikes' do
-    bike = Bike.new
+    allow(bike). to receive_messages(
+    :class => Bike,
+    :working? => true,
+    :report_broken => nil
+    )
     subject.dock(bike, true)
     expect{subject.release_bike}.to raise_error('No bikes available')
   end
+
 
 end
